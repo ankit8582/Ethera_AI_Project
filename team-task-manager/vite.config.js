8e388495e5ecc6d46020167e45736243ac038fc7
ankit8582 <124858807+ankit8582@users.noreply.github.com>
2026-05-07 10:26:04 +0530

    fix refresh error

--- team-task-manager/vite.config.js
+++ team-task-manager/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react({
+      fastRefresh: command === 'serve',
+    }),
+    tailwindcss(),
+  ],
   server: {
     allowedHosts: [
       'etheraaiproject-production.up.railway.app'
     ]
   }
-})
+}))
